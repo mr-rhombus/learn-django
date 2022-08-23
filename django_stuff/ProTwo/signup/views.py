49812024afa1abe77c086a8aa4223bfaaa29a8ea
Max Diamond <diamond_max@bah.com>
2022-08-23 18:25:28 -0400

    Create signup page view

--- django_stuff/ProTwo/signup/views.py
+++ django_stuff/ProTwo/signup/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from . import signup_form
+from signup.forms import SignupForm
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 def signup(request):
-    form = signup_form.SignupForm()
+    form = SignupForm()
 
     if request.method == 'POST':
-        form = signup_form.SignupForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
-            # form.save()
-            print('Validation successful!')
-            print('Email:\t', form.cleaned_data['email'])
-            print('Password:\t', form.cleaned_data['password'])
-
+            try:
+                form.full_clean()
+                form.save()
+            except:
+                raise ValidationError('Please enter an email that is not already aligned to an account!')
 
     return render(request, 'signup/signup.html', {'form': form})
