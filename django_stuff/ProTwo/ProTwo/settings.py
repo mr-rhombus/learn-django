b7d722f1ab95d1c5a484b1e7084425ccfd5df6bd
Max Diamond <diamond_max@bah.com>
2022-08-23 18:26:11 -0400

    Added crispy to INSTALLED_APPS and imported templates

--- django_stuff/ProTwo/ProTwo/settings.py
+++ django_stuff/ProTwo/ProTwo/settings.py
@@ -41,8 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'AppTwo',
     'django_seed',
-    'signup.apps.SignupConfig'
+    'signup.apps.SignupConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
