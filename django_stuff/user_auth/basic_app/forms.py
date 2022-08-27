c4a3c57c43ee2ad8c873d9fc21eeff58f8d88dad
Max Diamond <diamax98@gmail.com>
2022-08-26 23:49:10 -0400

    Trying to link 2 forms together for a registration page

--- django_stuff/user_auth/basic_app/forms.py
+++ django_stuff/user_auth/basic_app/forms.py
@@ -1,10 +1,15 @@
-from socket import fromshare
 from django import forms
 from django.contrib.auth.models import User
 from basic_app.models import UserProfileInfo
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
 
     class Meta():
         model = User
@@ -12,6 +17,16 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileInfoForm(forms.ModelForm):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
+    
     class Meta():
         model = UserProfileInfo
-        fields = ('portfolio_site', 'profile_pic')
+        fields = ('username', 'email', 'password', 'portfolio_site', 'profile_pic')
--- django_stuff/user_auth/basic_app/views.py
+++ django_stuff/user_auth/basic_app/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from basic_app.forms import UserForm, UserProfileInfoForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'basic_app/index.html')
+
+
+def register(request):
+    # Assume user is not registered
+    registered = False
+    # POST request
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+        # if user_form.is_valid() and profile_form.is_valid():
+        if profile_form.is_valid():
+            # user form
+            user = user_form.save()
+            user.set_password(user.password)  # hash the password
+            user.save()
+            # profile form
+            # Get profile info from form, but don't commit yet so we don't get collision errors by trying to save over existing user in user_form
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            # request.FILES is a dict of files a user uploads during the request
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+            profile.save()
+            registered=True
+        else:
+            print('invalid')
+            print(user_form.errors, profile_form.errors)  # print out the errors for the form(s)
+    # HTTP request
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+        return render(request, 'basic_app/register.html', 
+            {
+                'user_form': user_form,
+                'profile_form': profile_form,
+                'registered': registered,
+            })
+
+
+def login(request):
+    return render(request, 'basic_app/login.html')
