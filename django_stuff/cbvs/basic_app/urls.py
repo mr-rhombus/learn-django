7a287a182683b0b5e29ee162a5a5a4d947f47389
Max Diamond <diamax98@gmail.com>
2022-08-27 22:28:15 -0400

    Created urls for index, list and detail views

--- django_stuff/cbvs/basic_app/urls.py
+++ django_stuff/cbvs/basic_app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from basic_app import views
 
+app_name = 'basic_app'
+
 urlpatterns = [
     # path('', views.index, name='index')  # function-based view
-    path('', views.CBView.as_view())  # class-based view
+    path('', views.IndexView.as_view(), name='index'),  # class-based view
+    # path('school_detail', views.SchoolDetailView.as_view(), name='school_detail'),
+    path('school_list', views.SchoolListView.as_view(), name='school_list'),
+    # on school_list view, if we click a school we are linked to that school's detail view. Below, we define the primary key that aligns to the school, which is called from {{ school.id }} in school_list.html
+    # path(r'school_detail/(?P<pk>\d+)/$', views.SchoolDetailView.as_view(), name='school_detail'),
+    path('school_detail/<int:pk>', views.SchoolDetailView.as_view(), name='school_detail'),
 ]
