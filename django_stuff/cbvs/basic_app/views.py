8df3fa7c6dbec4e2ec78375728aaad36c8f290d4
Max Diamond <diamax98@gmail.com>
2022-08-29 08:28:44 -0400

    Added delete view

--- django_stuff/cbvs/basic_app/views.py
+++ django_stuff/cbvs/basic_app/views.py
@@ -2,8 +2,8 @@ from pipes import Template
 from django.shortcuts import render
 from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from typing import Any, Dict
-from basic_app import models
-from basic_app import forms
+from basic_app import models, forms
+from django.urls import reverse_lazy
 
 # Create your views here.
 ## Function-based view
@@ -32,7 +32,6 @@ class SchoolDetailView(DetailView):
     # If we don't define context_object_name, the DetailView returns a context dict called school (lowercase of model name)
     template_name = 'basic_app/school_detail.html'
 
-
 class SchoolCreateView(CreateView):
     model = models.School
     # fields = ('name', 'principal', 'location')
@@ -40,7 +39,12 @@ class SchoolCreateView(CreateView):
     # template_name = 'basic_app/create_school.html'
     ## default template_name is 'basic_app/school_form.html'
 
-
 class SchoolUpdateView(UpdateView):
     model = models.School
     fields = ('name', 'principal')
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    # Once you successfully delete, go back to school list view
+    # Evaluated lazily since we only want it eval'd when called
+    success_url = reverse_lazy('basic_app:school_list')
